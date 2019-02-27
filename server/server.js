8c58a0c895958c47696d3ea231081e9d69f7a03f
Theophile Avoyne <tavoyne@gmail.com>
2019-02-27 10:02:57 +0000

    Add test for GET /todos

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ app.listen(3000, () => {
 
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
-    res.send({todos})
+    res.send({todos});
   }, (e) => {
     res.status(400).send(e);
   });
