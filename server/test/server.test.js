d109fcff091b7ee2d01aa38b132a4b70520f17bc
Theophile Avoyne <tavoyne@gmail.com>
2019-02-28 20:39:59 +0000

    Add test for DELETE /posts/:id route

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -103,3 +103,40 @@ describe('GET /todos/:id', () => {
       .end(done);
   });
 });
+
+describe('DELETE /todos/:id', () => {
+  it('Should remove a todo', (done) => {
+    var hexId = todos[1]._id.toHexString();
+
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo._id).toBe(hexId);
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        };
+
+        Todo.findById(hexId).then((todo) => {
+          expect(todo).toNotExist;
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+
+  it('Should return a 404 if todo not found', (done) => {
+    request(app)
+      .delete(`/todos/${new ObjectID().toHexString()}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('Should return a 404 for non-valid ids', (done) => {
+    request(app)
+      .delete(`/todos/123abc`)
+      .expect(404)
+      .end(done);
+  });
+});
