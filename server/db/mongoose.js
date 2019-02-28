475e09b5bb8f3cda8eef76de1d1ff1bcaea9bbb6
Theophile Avoyne <tavoyne@gmail.com>
2019-02-28 21:55:20 +0000

    Solved FindByIdAndUpdate depreciation warning

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,9 @@
 mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true});
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
 module.exports = {mongoose};
