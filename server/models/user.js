bd38223423823fe17ab769a85ff00747bfd646f8
Theophile Avoyne <tavoyne@gmail.com>
2019-03-01 18:31:44 +0000

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -89,6 +89,16 @@ UserSchema.statics.findByCredentials = function(email, password) {
   });
 };
 
+UserSchema.methods.removeToken = function (token) {
+  var user = this;
+
+  return user.update({
+    $pull: {
+      tokens: {token}
+    }
+  });
+};
+
 UserSchema.pre('save', function(next) {
   var user = this;
 
