f50752c7a6dbc42a72e73538860fb2c83f2c49c1
Theophile Avoyne <tavoyne@gmail.com>
2019-03-01 16:15:32 +0000

    Hash user password before saving

--- server/middleware/authenticate.js
+++ server/middleware/authenticate.js
@@ -1,6 +1,6 @@
-var {User} = require('./../models.users');
+var {User} = require('./../models/user');
 
-var authenticate = (req, res, next) {
+var authenticate = (req, res, next) => {
   var token = req.header('x-auth');
 
   User.findByToken(token).then((user) => {
